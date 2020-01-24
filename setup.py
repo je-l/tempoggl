45b999f086712c0cd3874f4eba1fd723d4c9900c
je-l <je-l@users.noreply.github.com>
2020-01-24 20:09:26 +0200

    Update dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     packages=find_packages('.'),
     install_requires=[
         'pyhumps < 3.0.0, >= 1.2.2',
-        'pydantic == 0.23',  # semver allows breaking changes in < 1.0.0
+        'pydantic >= 1.0.0, < 2.0.0',
         'requests < 3.0.0, >= 2.21.0',
         'python-dateutil < 3.0.0, >= 2.0',
         'tzlocal < 3.0.0, >= 2.0.0',
